c8447fdc2010d36d3aaf8497a890cf864e43ccb0
albert <albertmart2002@gmail.com>
2023-07-27 12:11:01 -0400

    Deleted Comment

--- PP.Library/Models/Client.cs
+++ PP.Library/Models/Client.cs
@@ -22,30 +22,5 @@ namespace PP.Library.Models
             return $"{Id} {Name}";
         }
 
-        /* public Client() //Default Constructor
-         {
-             Console.WriteLine("Enter the client's name: ");
-             var name = Console.ReadLine() ?? string.Empty;
-
-             Console.WriteLine("Enter a Client ID: ");
-             var id = int.Parse(Console.ReadLine() ?? "0");
-
-             Console.WriteLine("Enter an opening date for client transactions in mm/dd/yyyy format: ");
-             var openDate = DateTime.Parse(Console.ReadLine() ?? DateTime.Today.ToString());
-
-             Console.WriteLine("Enter any notes you wish to add for the client.");
-             var notes = Console.ReadLine() ?? string.Empty;
-
-             bool active = true;
-
-             Name = name;
-             Id = id;
-             OpenDate = openDate;
-             Notes = notes;
-             IsActive = active;
-             ClientProjects = new List<Project>();
-
-         }*/
-
     }
 }
