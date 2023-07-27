93b7ec80a372f4f7724c21be58cf82b8ed075152
albert <albertmart2002@gmail.com>
2023-07-27 13:57:34 -0400

    Added Web API Project to PracticePanther

---
+++ PP.API/PP.API/Controllers/ClientController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PP.API.Database;
+using PP.Library.Models;
+
+namespace PP.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController : ControllerBase
+    {
+        private readonly ILogger<ClientController> _logger;
+
+        public ClientController(ILogger<ClientController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Client> Get()
+        {
+            return FakeDatabase.Clients;
+        }
+    }
+}
---
+++ PP.API/PP.API/Database/FakeDatabase.cs
@@ -0,0 +1,18 @@
+﻿using PP.Library.Models;
+
+namespace PP.API.Database
+{
+    public static class FakeDatabase
+    {
+        public static List<Client> Clients = new List<Client>()
+            {
+                new Client{Id = 1, OpenDate = DateTime.Now, IsActive = false, Name = "Client 1", Notes = "FirstClient"},
+                new Client{Id = 2, OpenDate = DateTime.Now, IsActive = false, Name = "Client 2", Notes = "SecondClient"},
+                new Client{Id = 3 , OpenDate = DateTime.Now, IsActive = false, Name = "Client 3", Notes = "ThirdClient"},
+                new Client{Id = 4 , OpenDate = DateTime.Now, IsActive = false, Name = "Client 4", Notes = "FourthClient"},
+                new Client{Id = 5 , OpenDate = DateTime.Now, IsActive = false, Name = "Client 5", Notes = "FifthClient"},
+                new Client{Id = 6 , OpenDate = DateTime.Now, IsActive = false, Name = "Client 6", Notes = "SixthClient" },
+                new Client{Id = 7 , OpenDate = DateTime.Now, IsActive = false, Name = "Client 7", Notes = "SeventhClient" },
+            };
+    }
+}
